55a59aeb761badea73488268486634831f4b8b91
calmjelly <707678143@qq.com>
2022-10-05 23:53:33 +0800

    二叉树5

---
+++ src/main/java/com/example/leetcode/btree/T700.java
@@ -0,0 +1,23 @@
+package com.example.leetcode.btree;
+
+import com.example.leetcode.common.TreeNode;
+
+/**
+ * @Description: 700. 二叉搜索树中的搜索
+ * @Author: Ybl
+ * @Date: 2022/10/5
+ */
+public class T700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null){
+            return null;
+        }
+        if (root.val == val){
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left,val);
+        }else {
+            return searchBST(root.right,val);
+        }
+    }
+}
