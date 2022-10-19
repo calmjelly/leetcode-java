d7d32927795764e7dcd6ff4f0f005bb6b3010e0e
calmjelly <707678143@qq.com>
2022-10-19 23:48:40 +0800

    买卖股票3和4

---
+++ src/main/java/com/example/leetcode/dp/T188.java
@@ -0,0 +1,29 @@
+package com.example.leetcode.dp;
+
+/**
+ * @Description: 188. 买卖股票的最佳时机 IV
+ * @Author: Ybl
+ * @Date: 2022年10月19日23:48:25
+ */
+public class T188 {
+    public int maxProfit(int k, int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2 * k + 1];
+        for (int i = 0; i < 2 * k + 1; i++) {
+            if (i % 2 == 0) {
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = -prices[0];
+            }
+        }
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+            }
+        }
+        return dp[prices.length - 1][2 * k];
+    }
+}
